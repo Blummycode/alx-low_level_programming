e6b20747495ef6fc121dda922fcf1cb291bb046e
Blummycode <simonresearcher@gmail.com>
2022-12-09 12:54:49 -0800

    alpahbet 2

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - main prints lower and upper case alphabet
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int c = 'a';
+	int C = 'A';
+
+	while(c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	while(C <= 'Z')
+	{
+		putchar(C);
+		C++;
+	}
+
+	putchar('\n');
+	return (0);
+}
