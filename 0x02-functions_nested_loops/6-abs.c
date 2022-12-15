cd539b960bb99217bac7af52e475bf41930504a2
Blummycode <simonresearcher@gmail.com>
2022-12-15 14:24:44 -0800

    6 abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - return absolute value of an int
+ * @n: the int to be checked
+ * Return: absolute value of int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
