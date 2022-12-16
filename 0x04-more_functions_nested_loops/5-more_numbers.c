0be2e41a6db8cb5d0063162b4ab9d7c290b9f676
Blummycode <simonresearcher@gmail.com>
2022-12-16 03:03:59 -0800

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,9 +9,12 @@ void more_numbers(void)
 
 	for (i = 0; i <= 10; i++)
 	{
-		if (j >= 10)
-			_putchar('1');
-		_putchar(j % 10 + '0');
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+				_putchar('1');
+			_putchar(j % 10 + '0');
+		}
 	}
 	_putchar('\n');
 }
