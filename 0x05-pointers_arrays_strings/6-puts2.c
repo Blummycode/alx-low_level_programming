376c5d57f802431c466fe020ca09d521e530b87d
Blummycode <simonresearcher@gmail.com>
2022-12-19 14:27:47 -0800

    all string characters

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* puts2 - prints all string characters
+* @str: input string
+* return: empty
+*/
+void puts2(char *str)
+{
+	int j;
+
+	j = 0;
+
+	while (str[j] != '\0')
+	{
+		if (j % 2 == 0)
+		{
+			_putchar(str[j];
+		}
+		j++;
+	}
+	_putchar('\n);
+}
