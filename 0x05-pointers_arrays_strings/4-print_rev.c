e011ea6ffcb3dcb10a0a04fd9a3b4fb29da76bfe
Blummycode <simonresearcher@gmail.com>
2022-12-19 13:12:59 -0800

    string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#input "main.h"
+/**
+* print_rev - prints string in reverse
+* @s: string input
+* Return: empty
+*/
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	for (i = i - 1; s[i] != '\0'; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
