352f42396aca6ce5dd10f2501993f93a621dcdd7
Blummycode <simonresearcher@gmail.com>
2022-12-22 03:52:40 -0800

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+* _strcmp - function for comparing two strings
+* @s1: pointer for first string to be compared
+* @s2: pointer for second string to be compared
+* return: empty
+*/
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+	
+	result = strcmp(s1, s2);
+	return (result);
+}
