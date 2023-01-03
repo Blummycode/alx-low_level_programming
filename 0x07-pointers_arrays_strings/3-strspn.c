389739f9c4e6ac1095db050797f17c00bae21856
Blummycode <simonresearcher@gmail.com>
2023-01-03 02:25:08 -0800

    length of a prefix

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+*_strspn - gets the length of a prefix substring
+*@s: pointer to the string to be searched
+*@accept: prefix measured
+*
+*Return: Always 0(success)
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	int n;
+
+	n = strspn(char *s, char *accept);
+	return (0);
+}
