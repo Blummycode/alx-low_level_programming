b9197dc736cd653a34be69295b44a429a2c2c7e3
Blummycode <simonresearcher@gmail.com>
2023-01-05 19:39:59 -0800

    compares 2 strings

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+*is_palindrome - returns 1 if string is palindrome and 0 if not
+*@s: pointer to the string to be checked
+*Return: 1 if string is plaindrome and 0 if not
+*/
+int is_palindrome(char *s)
+{
+
