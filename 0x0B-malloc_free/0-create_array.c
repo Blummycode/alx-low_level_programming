b41cabf4ccf38a9e20b64fbe92e0c71689e3d632
Blummycode <simonresearcher@gmail.com>
2023-01-11 08:17:13 -0800

    array of chars

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,10 +10,10 @@
 char *create_array(unsigned int size, char c)
 {
 	char *str;
-	int i, n;
+	unsigned int i;
 
-	str = malloc(n * sizeof(char));
-	if (n == 0 || str == NULL)
+	str = malloc(size * sizeof(char));
+	if (size == 0 || str == NULL)
 		return (NULL);
 	for (i = 0; i < n; i++)
 		str[i] = c;
