817ec4f59d340e94ad2a5931313608391a80199b
Blummycode <simonresearcher@gmail.com>
2023-01-12 05:51:26 -0800

    string concatenation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,7 +12,8 @@ char *str_concat(char *s1, char *s2)
 {
 	char *s;
 	unsigned int i = 0, j = 0, 
-	length1 = 0; length2 = 0;
+	unsigned int length1 = 0;
+        unsigned int length2 = 0;
 
 	while (s1 && s1[length1])
 		length1++;
