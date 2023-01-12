b51b919a3288d1a795e8e57aab88922defda6760
Blummycode <simonresearcher@gmail.com>
2023-01-12 06:50:57 -0800

    alloc grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -20,7 +20,7 @@ int **alloc_grid(int width, int height)
 	{
 		ptr = (int **) malloc(height * sizeof(int *));
 
-		if (!ptr == NULL)
+		if (ptr == NULL)
 			return (NULL);
 		for (i = 0; i < height; i++)
 		{
