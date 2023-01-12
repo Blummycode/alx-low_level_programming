fcb72f8210ee220059dbaea9728378cf96814b41
Blummycode <simonresearcher@gmail.com>
2023-01-12 01:41:57 -0800

    duplicate string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -7,17 +7,16 @@
 */
 char *_strdup(char *str)
 {
-char *ptr;
-unsigned int size, i;
-
-if (str == NULL)
-	return (NULL);
-for (size = 0; str[size] != '\0'; size++)
-ptr = (char *) malloc((size + 1) * sizeof(char));
-if (ptr == NULL)
-	return (NULL);
-for (i = 0; str[i] != '\0'; i++)
+	char *ptr;
+	unsigned int size, i;
+	if (str == NULL)
+		return (NULL);
+	for (size = 0; str[size] != '\0'; size++)
+	ptr = (char *) malloc((size + 1) * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
 	ptr[i] = str[i];
 	ptr[i] = '\0';
-return (ptr);
+	return (ptr);
 }
