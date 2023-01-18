a574483e4d6428ab790ce81b9e120cf6f90278ec
Blummycode <simonresearcher@gmail.com>
2023-01-18 06:32:30 -0800

    initialize poppy

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+*init_dog - initializes a variable of type struct dog
+*@d: pointer to the dog to be initialized
+*@name: name of the dog to be initialized
+*@age: age of the dog to be initialized
+*@owner: owner of dog to be initialized
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
