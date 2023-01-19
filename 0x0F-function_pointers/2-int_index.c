fa27d9e93feb7124e83f5884f4d8bce75f0593b3
Blummycode <simonresearcher@gmail.com>
2023-01-19 11:43:43 -0800

    find interger

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+/**
+*int_index - searches for an interger
+*@array: the collection of elements
+*@size: number of elements in the array
+*@cmp: pointer to the function
+*Return: Index of the first element
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL || size <== 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+		{
+			return (i);
+		}
+		return (-1);
