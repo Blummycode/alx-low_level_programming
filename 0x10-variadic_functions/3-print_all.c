bff5c18f0b7a084319399ba7fa3dde9d89194d56
Blummycode <simonresearcher@gmail.com>
2023-01-20 10:09:24 -0800

    prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -13,5 +13,35 @@ void print_all(const char * const format, ...)
 	char *s, *separator;
 
 	va_start(args, format);
+	separator = "";
 
-
+	i = 0;
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%s%c", separator, va_arg(args, int));
+				break;
+			case 'i':
+				printf("%s%d", separator, va_arg(args, int));
+				break;
+			case 'f':
+				printf("%s%f", separator, va_arg(args, double));
+				break;
+			case 's': 
+				s = va_arg(args, char *);
+				if (s == NULL)
+					printf("nil");
+				printf("%s%s", separator, s);
+				break;
+			default:
+				i++;
+				continue;
+		}
+		separator = ",";
+		i++;
+	}
+	printf("\n");
+	va_end(args);
+}
