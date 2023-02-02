eb112bfd988bdba6e33aa9afebc06a413cd50bb4
Blummycode <simonresearcher@gmail.com>
2023-02-02 05:32:14 -0800

    free listint

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees a list
+ * @head: a pointer to the listint_t
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp
+	if (head == NULL)
+		return;
+	while (*head)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+	*head = NULL;
+}
